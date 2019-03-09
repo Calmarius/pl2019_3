daae508aab29912479050dd2b93d315ce1bb4c14
Anonymous <>
2019-03-09 18:18:10 +0000

    Moved libpfx files to its own folder. Also created a makefile for it.
    Signed-off-by: Anonymous <>

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+
 g++ -g -O0 -Wall -Wextra -fmax-errors=1 _.cpp main.cpp -o main
--- build_lib.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-g++ -c -g -O0 -Wall -Wextra -fmax-errors=1 _.cpp -o _.o
-ar rcs libpfx.a _.o
-
